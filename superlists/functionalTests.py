1271ecd0e148071f8b51945d37f82e416363490e
m516 <m516@m516>
2016-01-19 14:02:45 +0800

    POST之後redirect，然後在範本中顯示所有項目.

--- superlists/functionalTests.py
+++ superlists/functionalTests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
+
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -12,13 +14,16 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox(profile)
         self.browser.implicitly_wait(3) 
         
+        
     def tearDown(self):
         self.browser.quit()
+        
         
     def check_for_row_in_listTable(self, rowText):
         table = self.browser.find_element_by_id('listTable')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(rowText, [row.text for row in rows])
+        
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 彤彤聽說有一款很酷的待辦事項應用程式，她前往它的首頁
@@ -37,6 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         )
         
         # 她在文字框裡輸入了「買孔雀羽毛」(她的嗜好是做路亞假餌Fly-fishing lure)
+        
         inputBox.send_keys('買孔雀羽毛')
         
         # 當她按下「送出」按鈕，頁面資訊更新，待辦事項清單裡多了一個項目：「買孔雀羽毛」
@@ -48,7 +54,6 @@ class NewVisitorTest(unittest.TestCase):
         inputBox.send_keys('利用孔雀羽毛來做一個路亞')
         inputBox.send_keys(Keys.ENTER)
         
-        
         # 頁面再次更新，現在待辦事項清單裡有兩個項目了
         self.check_for_row_in_listTable('買孔雀羽毛')
         self.check_for_row_in_listTable('利用孔雀羽毛來做一個路亞')
@@ -59,5 +64,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她前往該URL，待辦清單依舊存在
         
         # 她很滿意，就上床睡覺了
+        
+        
 if __name__=='__main__':
     unittest.main(warnings='ignore')
